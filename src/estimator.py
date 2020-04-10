acd28d86027b50d87cd933b7ae2405ec01347b08
kalycoding <muhdkhalifa1@yahoo.com>
2020-04-10 16:26:22 +0200

    second commit

--- src/estimator.py
+++ src/estimator.py
@@ -95,6 +95,20 @@ def estimator(data):
   }
   return impact
 
-estimator(data)
+(estimator(data = {
+  "data": {
+    "region": {
+      "name": "Africa",
+      "avgAge": 19.7,
+      "avgDailyIncomeInUSD": 4,
+      "avgDailyIncomePopulation": 0.73
+    },
+    "periodType": "days",
+    "timeToElapse": 38,
+    "reportedCases": 2747,
+    "population": 92931687,
+    "totalHospitalBeds": 678874
+  }
+})
 
 
