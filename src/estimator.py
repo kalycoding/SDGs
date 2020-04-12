10706d434d6387ada9d8847012ee004d094d9253
kalycoding <muhdkhalifa1@yahoo.com>
2020-04-13 00:23:47 +0200

    challenge 1

--- src/estimator.py
+++ src/estimator.py
@@ -19,6 +19,16 @@ def estimator(data):
                 factor = days // 3
             self.infectByTime = self.currentlyInf * (2 ** factor)
             return self.infectByTime
+        def SevereCasesByRequestedTime(self):
+            return int(0.15 * self.infectByTime)
+        def hospitalBedsByRequestedTime(self, bed):
+            return int(0.35 * (bed - self.infectByTime * 0.15))
+        def casesForICUByRequestedTime(self):
+            return int(0.05 * self.infectByTime)
+        def casesForVentilatorsByRequestedTime(self):
+            return int(0.02 * self.infectByTime)
+        def dollarsInFlight(self, population, dollar, days):
+            return (self.infectByTime * population * dollar) / 30
 
     class SevereImpact:
         def __init__(self):
@@ -38,16 +48,37 @@ def estimator(data):
                 factor = days // 3
             self.infectByTime = self.currentlyInf * (2 ** factor)
             return self.infectByTime
+        def SevereCasesByRequestedTime(self):
+            return int(0.15 * self.infectByTime)
+        def hospitalBedsByRequestedTime(self, bed):
+            return int(0.35 * (bed - self.infectByTime * 0.15))
+        def casesForICUByRequestedTime(self):
+            return int(0.05 * self.infectByTime)
+        def casesForVentilatorsByRequestedTime(self):
+            return int(0.02 * self.infectByTime)
+        def dollarsInFlight(self, population, dollar, days):
+            return (self.infectByTime * population * dollar) / 30
+        
     impact = Impact()
     severe = SevereImpact()
     data = {
             "impact": {
                 "currentlyInfected": impact.currentlyInfected(data['reportedCases']),
                 "infectionsByRequestedTime": impact.infectionsByRequestedTime(data['timeToElapse']),
+                "severeCasesByRequestedTime": impact.SevereCasesByRequestedTime(),
+                "hospitalBedsByRequestedTime": impact.hospitalBedsByRequestedTime(data['totalHospitalBeds']),
+                "casesForICUByRequestedTime": impact.casesForICUByRequestedTime(),
+                "casesForVentilatorsByRequestedTime": impact.casesForVentilatorsByRequestedTime(),
+                "dollarsInFlight": impact.dollarsInFlight(data['region']['avgDailyIncomePopulation'],data['region']['avgDailyIncomeInUSD'], data['timeToElapse'])
             },
             "severeImpact": {
                 "currentlyInfected": severe.currentlyInfected(data['reportedCases']),
                 "infectionsByRequestedTime": severe.infectionsByRequestedTime(data['timeToElapse']),
+                "severeCasesByRequestedTime": severe.SevereCasesByRequestedTime(),
+                "hospitalBedsByRequestedTime": severe.hospitalBedsByRequestedTime(data['totalHospitalBeds']),
+                "casesForICUByRequestedTime": severe.casesForICUByRequestedTime(),
+                "casesForVentilatorsByRequestedTime": severe.casesForVentilatorsByRequestedTime(),
+                "dollarsInFlight": severe.dollarsInFlight(data['region']['avgDailyIncomePopulation'],data['region']['avgDailyIncomeInUSD'], data['timeToElapse'])
             }
         
     }
@@ -67,5 +98,5 @@ data = {
     "population": 92931687,
     "totalHospitalBeds": 678874
 }
-estimator(data)
+print(estimator(data))
 
